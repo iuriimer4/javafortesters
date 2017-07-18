98c819fc408b30786552cfd459b4f87feda66d59
iuriimer <iurii.merviak@yahoo.com>
2017-07-18 18:32:23 -0500

    new commit

---
+++ Users/Iurii/javafortesters/src/test/java/com/javafortesters/LoopClass.java
@@ -0,0 +1,18 @@
+package com.javafortesters;
+
+/**
+ * Created by Iurii on 7/16/2017.
+ */
+public class LoopClass {
+       /*public static void main(String[] args) {
+            int x = 3;
+            while (x > 0) {
+                System.out.println(x);*/
+
+    public static void main(String[] args) {
+        int a = 3126;
+        int b = 8;
+        int c = a - b;
+        System.out.println(c + "");
+    }
+}
