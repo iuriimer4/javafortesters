dd99ac066b83dbcde967d3f4adc404f935048d49
iuriimer <iurii.merviak@yahoo.com>
2017-07-18 18:49:07 -0500

    new commit

--- Users/Iurii/javafortesters/src/test/java/com/javafortesters/LoopClass.java
+++ Users/Iurii/javafortesters/src/test/java/com/javafortesters/LoopClass.java
@@ -10,7 +10,7 @@ public class LoopClass {
                 System.out.println(x);*/
 
     public static void main(String[] args) {
-        int a = 3126;
+        /int a = 3126;
         int b = 8;
         int c = a - b;
         System.out.println(c + "");
